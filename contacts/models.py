ea52f19ad61708b69acedca6baa74b37072619f4
Ayobamidele <gethelp.revayah@gmail.com>
2022-10-23 05:30:23 +0100

    Search functionalities

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length = 300)
+    relationship = models.CharField(max_length = 300)
+    address = models.CharField(max_length = 300)
+    email = models.EmailField(max_length = 250)
+    phone_number = models.CharField(max_length = 250)
+
+
+    def __str__(self):
+
+        return self.full_name
